874a8af3ade5e5ad4715cd50a54fb442fe44803c
M. Goidin Yves <goidin@geitp105-07.insa-toulouse.fr>
2014-01-14 09:11:39 +0100

    noeud Kalman

---
+++ ardrone_kalmanNode/src/ardrone_kalmanNode/msg/__init__.py
@@ -0,0 +1,1 @@
+from ._Ardrone_KalmanPos import *
---
+++ ardrone_kalmanNode/src/kalmanNode.cpp
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+// %Tag(FULLTEXT)%
+// %Tag(ROS_HEADER)%
+#include "ros/ros.h"
+// %EndTag(ROS_HEADER)%
+// %Tag(MSG_HEADER)%
+#include "std_msgs/String.h"
+#include "geometry_msgs/Point.h"
+
+// %EndTag(MSG_HEADER)%
+
+#include <sstream>
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line. For programmatic
+   * remappings you can use a different version of init() which takes remappings
+   * directly, but for most command-line programs, passing argc and argv is the easiest
+   * way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+// %Tag(INIT)%
+  ros::init(argc, argv, "kalmanNode");
+// %EndTag(INIT)%
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+// %Tag(NODEHANDLE)%
+  ros::NodeHandle n;
+// %EndTag(NODEHANDLE)%
+
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+// %Tag(PUBLISHER)%
+  //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher KalmanPos_pub = n.advertise<geometry_msgs::Point>("Ardrone_KalmanPos", 10);
+// %EndTag(PUBLISHER)%
+
+// %Tag(SUBSCRIBER)%
+  //ros::Subscriber IMU_sub = n.subscribe("/ardrone/imu/data", 1000 /*, "function name to call" */);
+  //ros::Subscriber ODOM_pub = n.subscribe("/mono_odometer/odometry", 1000 /*, "function name to call" */);
+  //ros::Subscriber GPS_pub = n.subscribe("/gps", 1000 /*, "function name to call" */);
+  //ros::Subscriber TAG_pub = n.subscribe("/tag", 1000 /*, "function name to call" */);
+// %EndTag(SUBSCRIBER)%
+
+
+
+// %Tag(LOOP_RATE)%
+// refresh rate : 1Hz 
+  ros::Rate loop_rate(1);
+// %EndTag(LOOP_RATE)%
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+// %Tag(ROS_OK)%
+  int count = 0;
+  while (ros::ok())
+  {
+// %EndTag(ROS_OK)%
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+// %Tag(FILL_MESSAGE)%
+    
+    //std_msgs::String msg;
+    //std::stringstream ss;
+    //ss << "hello world " << count;
+    //msg.data = ss.str();
+    
+    //ardrone_KalmanNode::Ardrone_KalmanPos positionMsg;
+    geometry_msgs::Point positionMsg;
+    positionMsg.x = 1;
+    positionMsg.y = 2;
+    positionMsg.z = 0;
+    
+    
+    
+    ROS_INFO("%s", "test");
+
+    KalmanPos_pub.publish(positionMsg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+
+   };
+
+  return 0;
+}
+// %EndTag(FULLTEXT)%
