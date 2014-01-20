a9b82e91cb6acf83c2faaf42e96ca8c028ce812f
Jules <waldhart@etud.insa-toulouse.fr>
2014-01-20 22:11:07 +0100

    added missing file for matlab

---
+++ ardrone_moves/matlab/data.m
@@ -0,0 +1,16 @@
+%% Donnees des fonctions de transfert
+p0=0.0014;
+r0=0.0014;
+p1=0.9994;
+r1=0.9996;
+p2=-2.5028e-4;
+r2=7.053e-5;
+p3=0.9994;
+r3=0.9995;
+y0=1.3696e-6;
+g0=0.0220;
+y1=0.9937;
+g1=0.9970;
+
+Te=5e-3; % n'est pas indiqu� dans la fiche a v�rifier
+Tsim=60;
